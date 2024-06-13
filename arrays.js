1d1f3697f756b711286175d28228bac77ff22829
Sagar998047 <sagarkb43@gmail.com>
2024-06-13 16:33:07 +0530

    modifications are done

--- arrays.js
+++ arrays.js
@@ -1,3 +1,4 @@
 let rawveggis=["tomatato","potato","onions","chilli","ginger"]
+   let veg=[1,2,3,4,5,6]
    // console.log(rawveggis)
-    console.log(rawveggis.slice(0,4))
+    console.log(rawveggis.concat (veg))
