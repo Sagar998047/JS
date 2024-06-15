8c5a2331645c65f66d26080f1fc36ec1ab50361b
Sagar998047 <sagarkb43@gmail.com>
2024-06-15 13:16:42 +0530

    xzzxcvv

--- first.js
+++ first.js
@@ -1,4 +1,4 @@
- /*fullname="test"
+ fullname="test"
  console.log(fullname);  
  isfollow ="true"
  console.log(isfollow);
@@ -7,9 +7,9 @@
  let b=8;
  let c=a+b;
  console.log(c);
- }*/
-/*isfollow="true";
-console.log(isfollow);*/
+ }
+isfollow="true";
+console.log(isfollow);
 const student ={
      fullname:"sagar",
      age:24,
@@ -20,4 +20,11 @@ student["fullname"]="sagar kb";
 student["age"]+1;
 console.log(student["fullname"]);
 console.log(student["age"]);
-console.log(student['cgpa']);
+const employee={
+     ename:'varun',
+     eid:101,
+     salary:10000,
+};
+console.log(employee['ename']);
+console.log(employee['eid']);
+ console.log(employee['salary']);
