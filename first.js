ec49a8fc186f782ccce3792c099936d50533097e
Sagar998047 <sagarkb43@gmail.com>
2024-06-15 12:35:47 +0530

    take a look at the modifications

---
+++ first.js
@@ -0,0 +1,23 @@
+ /*fullname="test"
+ console.log(fullname);  
+ isfollow ="true"
+ console.log(isfollow);
+ {
+ let a=5;
+ let b=8;
+ let c=a+b;
+ console.log(c);
+ }*/
+/*isfollow="true";
+console.log(isfollow);*/
+const student ={
+     fullname:"sagar",
+     age:24,
+     cgpa:7.08,
+     ispass: true,
+};
+student["fullname"]="sagar kb";
+student["age"]+1;
+console.log(student["fullname"]);
+console.log(student["age"]);
+console.log(student['cgpa']);
