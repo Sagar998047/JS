9312fffe4a1d56ceddf9aa62c5d291fc1f198842
Sagar998047 <sagarkb43@gmail.com>
2024-06-16 23:36:13 +0530

    hxsvh

---
+++ operators.js
@@ -0,0 +1,28 @@
+
+//artimetic operator
+let a=5;
+let b=2;
+let x=a+b;
+console.log(x); 
+console.log('a-b =',a-b);
+console.log('a%b = ',a % b);
+console.log('a**b = ',a ** b);//5 power of 2
+ 
+//unary operator
+let c=5;
+let d=4;
+console.log('c=',c,'& d=',d);
+c=c+1;
+console.log('c=',c);
+
+// assignment operators
+c+=5;
+console.log('c=',c);
+d+=4;
+console.log('d=',d);
+
+// ternaryOperators
+let age=25;
+let result= age>=18? 'adult':'not adult';
+console.log(result);
+     
